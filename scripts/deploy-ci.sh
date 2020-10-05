a2e63d26f2451e103c3fa4b5ec3a73f21b363a8a
krzysztofreczek <reczek.krzysztof@gmail.com>
2020-10-05 21:36:10 +0200

    Fix script path

--- scripts/deploy-ci.sh
+++ scripts/deploy-ci.sh
@@ -9,17 +9,17 @@ echo "${KUBERNETES_CLUSTER_CERTIFICATE}" | base64 --decode > cert.crt
 K8S_NAMESPACE=nhood-prod
 
 kubectl apply \
-  --kubeconfig=/dev/null \
-  --server=${KUBERNETES_SERVER} \
-  --certificate-authority=cert.crt \
-  --token=${KUBERNETES_TOKEN} \
-  -f ./manifests/nhood-svc-deployment.yaml \
-  -n ${K8S_NAMESPACE}
+    --kubeconfig=/dev/null \
+    --server=${KUBERNETES_SERVER} \
+    --certificate-authority=cert.crt \
+    --token=${KUBERNETES_TOKEN} \
+    -f ./manifests/nhood-svc-deployment.yaml \
+    -n ${K8S_NAMESPACE}
 
 kubectl apply \
-  --kubeconfig=/dev/null \
-  --server=${KUBERNETES_SERVER} \
-  --certificate-authority=cert.crt \
-  --token=${KUBERNETES_TOKEN} \
-  -f ./manifests/nhood-svc-service.yaml \
-  -n ${K8S_NAMESPACE}
+    --kubeconfig=/dev/null \
+    --server=${KUBERNETES_SERVER} \
+    --certificate-authority=cert.crt \
+    --token=${KUBERNETES_TOKEN} \
+    -f ./manifests/nhood-svc-service.yaml \
+    -n ${K8S_NAMESPACE}
